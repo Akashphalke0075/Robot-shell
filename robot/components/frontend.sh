611f6031f29a52ddb58b6928c54c5f81b8c74474
Akash <phalke.aka@gmail.com>
2023-03-03 15:00:44 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -20,6 +20,11 @@ fi
 
 echo "downloading the component"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+if [ $? -eq 0 ]; then
+echo -e "\e[32m success \e[0m"
+else
+echo -e "\e[31m Failure \e[0m"
+fi
 
 echo "cleaning up"
 rm -rf /usr/share/nginx/html/*  &>> /tmp/frontend.log
@@ -28,8 +33,25 @@ unzip /tmp/frontend.zip  &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
+if [ $? -eq 0 ]; then
+echo -e "\e[32m success \e[0m"
+else
+echo -e "\e[31m Failure \e[0m"
+fi
+
+echo "setting up proxy"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+if [ $? -eq 0 ]; then
+echo -e "\e[32m success \e[0m"
+else
+echo -e "\e[31m Failure \e[0m"
+fi
 
 echo "starting nginx"
 systemctl enable nginx  &>> /tmp/frontend.log
 systemctl start nginx  &>> /tmp/frontend.log
+if [ $? -eq 0 ]; then
+echo -e "\e[32m success \e[0m"
+else
+echo -e "\e[31m Failure \e[0m"
+fi
