fa0a6c35f64908ef3c8f39fffa2210c49d48879b
Akash <phalke.aka@gmail.com>
2023-03-04 00:21:44 +0530

    upadte

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
-echo "i am catalogue"
+set -e
+
+COMPONENT=catalogue
+APPUSER=roboshop
+source components/common.sh
+
+echo -n "configuring repo:"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
+stat $?
+ 
+echo -n "installing nodejs:" 
+yum install nodejs -y  &>> $LOGFILE
+stat $?
+
+echo -n "creating app user:" 
+useradd $APPUSER
+stat $?
+
+echo -n "downloading component:" 
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+stat $?
+
+echo -n "moving component to dirctory:"
+cd /home/$APPUSER
+unzip -o /tmp/catalogue.zip  &>> $LOGFILE
+mv $COMPONENT-main $COMPONENT
+stat $?
+
+echo -n "installing nodejs dependencies:"
+cd $COMPONENT
+npm install &>> $LOGFILE
+
