c548cc20a72ff6a31b1a157f54340d25d8249558
Akash <phalke.aka@gmail.com>
2023-03-03 13:52:35 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-USER=$(id -u)
-if [ $user -ne 0 ]; then
-echo "you need to have sudo access"
-
-yum install nginx -y
-
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
-rm -rf /usr/share/nginx/html/*
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
-systemctl enable nginx
-systemctl start nginx
+echo "this is frontend"
