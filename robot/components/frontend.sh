290c2bb597565e5f817323fcf95651d6b55df45f
Akash <phalke.aka@gmail.com>
2023-03-03 14:57:05 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -11,21 +11,25 @@ fi
 
 
 echo "installing nginx"
-yum install nginx -y
-
+yum install nginx -y  &>> /tmp/frontend.log
+if [ $? eq 0 ]; then
+echo -e "\e[32m success \e[0m"
+else
+echo -e "\e[31m Failure \e[0m"
+fi
 
 echo "downloading the component"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 echo "cleaning up"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/*  &>> /tmp/frontend.log
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip  &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 echo "starting nginx"
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx  &>> /tmp/frontend.log
+systemctl start nginx  &>> /tmp/frontend.log
