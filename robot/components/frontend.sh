78932d7407506dae0fd80b909d5eb0072a023541
Akash <phalke.aka@gmail.com>
2023-03-03 12:28:46 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -10,7 +10,7 @@ exit 1
 fi
 
 
-yum install nginx -y
+yum install nginx -y  &>> /tmp/frontend.log
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
@@ -19,11 +19,11 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend
 
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>> /tmp/frontend.log
+systemctl start nginx &>> /tmp/frontend.log
