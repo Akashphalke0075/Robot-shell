ec2c66c232929cf191ca09b4a55159900c8791af
Akash <phalke.aka@gmail.com>
2023-03-03 12:38:07 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -9,26 +9,36 @@ echo -e "\e[31m you must run as root user \e[0m]"
 exit 1
 fi
 
+
+
 echo "installing nginx"
 yum install nginx -y  &>> /tmp/frontend.log
+if [ $? eq 0 ]; then
+echo -e "\e[32m success\e[0m]"
+else
+echo -e "\e[32m failure\e[0m]"
 
-echo "downloading component"
-
+echo "downloading component:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+echo -e "\e[32m success\e[0m]"
+else
+echo -e "\e[32m failure\e[0m]"
 
-
-echo "performing cleanup"
-
+echo "performing cleanup:"
 rm -rf /usr/share/nginx/html/*
+echo -e "\e[32m success\e[0m]"
+else
+echo -e "\e[32m failure\e[0m]"
+
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 
-echo "setting up proxy"
+echo "setting up proxy:"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-echo "starting nginx"
+echo "starting nginx:"
 systemctl enable nginx &>> /tmp/frontend.log
 systemctl start nginx &>> /tmp/frontend.log
