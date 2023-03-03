0b8a7abcab40d75a57b2c56512a648a90900532e
Akash <phalke.aka@gmail.com>
2023-03-03 16:24:41 +0530

    update

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -5,3 +5,28 @@ set -e
 COMPONENT=mongodb
 
 source components/common.sh
+
+echo -n "configuring repo:"
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
+stat $?
+
+echo -n "installing ${COMPONENT}:"
+yum install -y mongodb-org
+stat $?
+
+
+
+
+# systemctl enable mongod
+# systemctl start mongod
+
+
+
+
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
