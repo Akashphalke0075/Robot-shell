4d36cd0a6fde9aee3b3863a0d0048f3706870b59
Akash <phalke.aka@gmail.com>
2023-03-03 13:04:19 +0530

    update

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -5,7 +5,7 @@ set -e
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-echo -e "\e[31m you must run as root user \e[0m]"
+echo -e "\e[31m you must run as root user \e[0m"
 exit 1
 fi
 
@@ -14,21 +14,21 @@ fi
 echo "installing nginx"
 yum install nginx -y  &>> /tmp/frontend.log
 if [ $? eq 0 ]; then
-echo -e "\e[32m success\e[0m]"
+echo -e "\e[32m success\e[0m"
 else
-echo -e "\e[32m failure\e[0m]"
+echo -e "\e[32m failure\e[0m"
 
 echo "downloading component:"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-echo -e "\e[32m success\e[0m]"
+echo -e "\e[32m success\e[0m"
 else
-echo -e "\e[32m failure\e[0m]"
+echo -e "\e[32m failure\e[0m"
 
 echo "performing cleanup:"
 rm -rf /usr/share/nginx/html/*
-echo -e "\e[32m success\e[0m]"
+echo -e "\e[32m success\e[0m"
 else
-echo -e "\e[32m failure\e[0m]"
+echo -e "\e[32m failure\e[0m"
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
