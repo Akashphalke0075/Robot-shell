ab644d6963cd505e76c1985d685e0da02c0ca6d1
Akash <phalke.aka@gmail.com>
2023-03-04 13:25:06 +0530

    update

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -7,11 +7,11 @@ APPUSER=roboshop
 source components/common.sh
 
 echo -n "\e[32m downoading dependencies \e[0m"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
 stat $?
 
 echo -n "\e[32m installing nodejs \e[0m"
-yum install nodejs -y 
+yum install nodejs -y  &>> $LOGFILE
 stat $?
 
 echo -n "\e[32m adding user \e[0m"
@@ -19,12 +19,12 @@ useradd $APPUSER
 stat $?
 
 echo -n "\e[32m downloading compo \e[0m"
-$ curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+$ curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip" &>> $LOGFILE
 stat $?
 
 echo -n "\e[32m switching and unzipping compo \e[0m"
 $ cd /home/$APPUSER
-$ unzip -o /tmp/$COMPONENT.zip
+$ unzip -o /tmp/$COMPONENT.zip  &>> $LOGFILE
 stat $?
 
 echo -n "\e[32m renaming file \e[0m"
@@ -33,5 +33,5 @@ $ cd /home/$APPUSER/$COMPONENT
 stat $?
 
 echo -n "\e[32m installing npm \e[0m"
-$ npm install
+$ npm install  &>> $LOGFILE
 stat $?
